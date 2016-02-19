4acde2ae9f741cdfe380be0cec9287eef13fed88
Jon Lund Steffensen <jonlst@gmail.com>
2016-02-19 15:04:47 -0500

    rpair: Better handle reactions with large stoichiometric values

--- psamm/rpair.py
+++ psamm/rpair.py
@@ -19,23 +19,26 @@ ATOM_WEIGHT = {
 def shared_elements(f1, f2):
     """Calculate score of shared elements."""
 
-    d1, d2 = {}, {}
-    for a, v in f1.flattened().items():
-        d1[a] = ATOM_WEIGHT.get(a, 1.0) * v
-    for a, v in f2.flattened().items():
-        d2[a] = ATOM_WEIGHT.get(a, 1.0) * v
+    d1 = dict(f1.flattened().items())
+    d2 = dict(f2.flattened().items())
 
     elements = set(element for element, _ in iteritems(d1))
     elements.update(element for element, _ in iteritems(d2))
 
     count, total = 0, 0
+    w_count, w_total = 0, 0
     for element in elements:
-        count += min(d1.get(element, 0), d2.get(element, 0))
-        total += max(d1.get(element, 0), d2.get(element, 0))
+        mi = min(d1.get(element, 0), d2.get(element, 0))
+        mx = max(d1.get(element, 0), d2.get(element, 0))
+        count += mi
+        total += mx
+        w_count += ATOM_WEIGHT.get(element, 1.0) * mi
+        w_total += ATOM_WEIGHT.get(element, 1.0) * mx
 
-    if total == 0.0:
-        return 0.0
-    return count / float(total)
+    score = 0.0 if total == 0.0 else count / float(total)
+    w_score = 0.0 if w_total == 0.0 else w_count / float(w_total)
+
+    return score, w_score
 
 
 def formula_common(f1, f2):
@@ -95,23 +98,53 @@ class _CompoundInstance(object):
 def predict_rpair(reaction, compound_formula):
     """Predict reaction pairs."""
 
-    transfer = {}
+    for _, value in reaction.compounds:
+        if not isinstance(value, int):
+            logger.warning('Reaction pairs are not available for non-integer'
+                           ' reaction {}'.format(reaction))
+            return {}, {}
 
-    def compound_instances(compounds):
-        for compound, value in compounds:
+    uninstantiated_left = dict(reaction.left)
+    uninstantiated_right = dict(reaction.right)
+
+    def compound_instances(uninstantiated):
+        instances = []
+        for compound, value in iteritems(uninstantiated):
+            if value > 0:
+                f = compound_formula[compound.name]
+                instances.append(_CompoundInstance(compound, value, f))
+
+        for inst in instances:
+            uninstantiated[inst.compound] -= 1
+
+        return instances
+
+    def instantiate(uninstantiated, compound):
+        n = uninstantiated[compound]
+        if n > 0:
             f = compound_formula[compound.name]
-            for i in range(value):
-                yield _CompoundInstance(compound, i, f)
+            inst = _CompoundInstance(compound, n, f)
+            uninstantiated[compound] -= 1
+            return inst
 
-    left = list(compound_instances(reaction.left))
-    right = list(compound_instances(reaction.right))
+        return None
+
+    left = compound_instances(uninstantiated_left)
+    right = compound_instances(uninstantiated_right)
     instances = left + right
 
     pairs = {}
     for inst1, inst2 in product(left, right):
-        pairs[inst1, inst2] = shared_elements(inst1.formula, inst2.formula)
+        score, w_score = shared_elements(inst1.formula, inst2.formula)
+        if score > 0.0:
+            pairs[inst1, inst2] = w_score
 
+    logger.debug('Reaction: {}'.format(reaction))
+
+    transfer = {}
     while len(pairs) > 0:
+        logger.debug('Pairs: {}'.format(pairs))
+
         (inst1, inst2), _ = max(iteritems(pairs), key=itemgetter(1))
         common = formula_common(inst1.formula, inst2.formula)
 
@@ -123,22 +156,47 @@ def predict_rpair(reaction, compound_formula):
         for inst in (inst1, inst2):
             inst.formula = formula_remove(inst.formula, common)
 
+        to_insert = set()
+
+        inst = instantiate(uninstantiated_left, inst1.compound)
+        if inst is not None:
+            left.append(inst)
+            instances.append(inst)
+            to_insert.add(inst)
+
+        inst = instantiate(uninstantiated_right, inst2.compound)
+        if inst is not None:
+            right.append(inst)
+            instances.append(inst)
+            to_insert.add(inst)
+
         to_update = {inst1, inst2}
+        logger.debug('To update: {}'.format(to_update))
+
         to_delete = set()
         for inst1, inst2 in pairs:
             if inst1 in to_update or inst2 in to_update:
                 if (len(dict(inst1.formula.items())) > 0 and
                         len(dict(inst2.formula.items())) > 0):
-                    shared = shared_elements(inst1.formula, inst2.formula)
-                    if shared == 0.0:
+                    score, w_score = shared_elements(
+                        inst1.formula, inst2.formula)
+                    if score == 0.0:
                         to_delete.add((inst1, inst2))
                     else:
-                        pairs[inst1, inst2] = shared
+                        pairs[inst1, inst2] = w_score
                 else:
                     to_delete.add((inst1, inst2))
 
+        logger.debug('To delete: {}'.format(to_delete))
         for pair in to_delete:
             del pairs[pair]
+
+        logger.debug('To insert: {}'.format(to_insert))
+        for inst1, inst2 in product(left, right):
+            if inst1 in to_insert or inst2 in to_insert:
+                score, w_score = shared_elements(inst1.formula, inst2.formula)
+                if score > 0.0:
+                    pairs[inst1, inst2] = w_score
 
     balance = {}
     for inst in instances:
